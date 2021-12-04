76fa44ec6d559d84d944417998361b10b8dca860
Serge Arbuzov <Serge.Arbuzov@spacebridge.com>
2021-12-04 12:14:45 +0300

    Initial commit
    Signed-off-by: Serge Arbuzov <info@whitediver.com>

---
+++ custom_components/delongi_primadonna/__init__.py
@@ -0,0 +1,24 @@
+"""
+The "hDelongi Primadonna" custom component.
+This component implements the bare minimum that a component should implement.
+Configuration:
+To use the hello_world component you will need to add the following to your
+configuration.yaml file.
+hello_world:
+"""
+from __future__ import annotations
+
+from homeassistant.core import HomeAssistant
+from homeassistant.helpers.typing import ConfigType
+
+# The domain of your component. Should be equal to the name of your component.
+DOMAIN = "delongi_primadonna"
+
+
+def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+    """Set up a skeleton component."""
+    # States are in the format DOMAIN.OBJECT_ID.
+    hass.states.set('hello_world.Hello_World', 'Works!')
+
+    # Return boolean to indicate that initialization was successfully.
+    return True
