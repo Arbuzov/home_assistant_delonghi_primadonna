e66068b981f19612d461735dd5538757c5e62ed2
ÐÐSerge Arbuzov <info@whitediver.com>
2023-03-05 00:17:49 +0300

    Sort imports
    Signed-off-by: Serge Arbuzov <info@whitediver.com>

--- custom_components/delonghi_primadonna/sensor.py
+++ custom_components/delonghi_primadonna/sensor.py
@@ -4,7 +4,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
-from .device import AvailableBeverage, DelonghiDeviceEntity, NOZZLE_STATE
+from .device import NOZZLE_STATE, AvailableBeverage, DelonghiDeviceEntity
 
 
 async def async_setup_entry(
