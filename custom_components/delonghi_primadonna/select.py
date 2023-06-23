8195eb0f152102b77dbd5b8dfb645066b5ef3a99
Serge Arbuzov <info@whitediver.com>
2023-06-23 15:54:29 +0300

    New input to make beverage
    Signed-off-by: Serge Arbuzov <info@whitediver.com>

--- custom_components/delonghi_primadonna/select.py
+++ custom_components/delonghi_primadonna/select.py
@@ -6,7 +6,7 @@ from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import AVAILABLE_PROFILES, DOMAIN
-from .device import DelonghiDeviceEntity, DelongiPrimadonna
+from .device import AvailableBeverage, DelonghiDeviceEntity, DelongiPrimadonna
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -16,13 +16,14 @@ async def async_setup_entry(
         async_add_entities: AddEntitiesCallback):
     delongh_device: DelongiPrimadonna = hass.data[DOMAIN][entry.unique_id]
     async_add_entities([
-        ProfileSelect(delongh_device, hass)
+        ProfileSelect(delongh_device, hass),
+        BeverageSelect(delongh_device, hass)
     ])
     return True
 
 
 class ProfileSelect(DelonghiDeviceEntity, SelectEntity):
-    """A select implementation for profile selection."""
+    """Implementation for profile selection."""
 
     _attr_name = 'Profile'
     _attr_options = list(AVAILABLE_PROFILES.keys())
@@ -33,3 +34,11 @@ class ProfileSelect(DelonghiDeviceEntity, SelectEntity):
         profile_id = AVAILABLE_PROFILES.get(option)
         self.hass.async_create_task(self.device.select_profile(profile_id))
         self._attr_current_option = option
+
+
+class BeverageSelect(DelonghiDeviceEntity, SelectEntity):
+    """Beverage start implementation by the select"""
+
+    _attr_name = 'Beverage'
+    _attr_options = [*AvailableBeverage]
+    _attr_current_option = [*AvailableBeverage][0]
