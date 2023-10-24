2871cd8ade84cfbb91852ca8a9bf59d0c0613331
Cody C <50791984+codyc1515@users.noreply.github.com>
2023-10-25 10:13:27 +1300

    Update sensor.py to support Config & Diagnostics

--- custom_components/delonghi_primadonna/sensor.py
+++ custom_components/delonghi_primadonna/sensor.py
@@ -3,7 +3,7 @@ from homeassistant.config_entries import ConfigEntry
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
-from .const import DOMAIN
+from .const import DOMAIN, ENTITY_CATEGORY
 from .device import DEVICE_STATUS, NOZZLE_STATE, DelonghiDeviceEntity
 
 
@@ -31,7 +31,12 @@ class DelongiPrimadonnaNozzleSensor(DelonghiDeviceEntity, SensorEntity):
     @property
     def native_value(self):
         return self.device.steam_nozzle
-
+    
+    @property
+    def entity_category(self, **kwargs: Any) -> None:
+        """Return the category of the entity."""
+        return EntityCategory.DIAGNOSTIC
+            
     @property
     def icon(self):
         result = 'mdi:coffee'
@@ -53,7 +58,12 @@ class DelongiPrimadonnaStatusSensor(DelonghiDeviceEntity, SensorEntity):
     @property
     def native_value(self):
         return self.device.status
-
+    
+    @property
+    def entity_category(self, **kwargs: Any) -> None:
+        """Return the category of the entity."""
+        return EntityCategory.DIAGNOSTIC
+    
     @property
     def icon(self):
         result = 'mdi:thumb-up-outline'
