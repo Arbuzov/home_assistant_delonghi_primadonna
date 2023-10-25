9c1e3a88a543b73171f9cd87f08017dbde43bca8
Cody C <50791984+codyc1515@users.noreply.github.com>
2023-10-25 18:18:06 +1300

    Fixes bools in binary_sensor.py

--- custom_components/delonghi_primadonna/binary_sensor.py
+++ custom_components/delonghi_primadonna/binary_sensor.py
@@ -63,7 +63,7 @@ class DelongiPrimadonnaDescaleSensor(DelonghiDeviceEntity, BinarySensorEntity):
     @property
     def icon(self):
         result = 'mdi:dishwasher'
-        if self.is_on():
+        if self.is_on:
             result = 'mdi:dishwasher-alert'
         return result
 
@@ -88,6 +88,6 @@ class DelongiPrimadonnaFilterSensor(
     @property
     def icon(self):
         result = 'mdi:filter'
-        if self.is_on():
+        if self.is_on:
             result = 'mdi:filter-off'
         return result
