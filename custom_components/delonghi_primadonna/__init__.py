ff3c3b032f279f856b6ef1005e68dde4db18ce11
Cody C <50791984+codyc1515@users.noreply.github.com>
2023-10-25 14:15:56 +1300

    Update __init__.py

--- custom_components/delonghi_primadonna/__init__.py
+++ custom_components/delonghi_primadonna/__init__.py
@@ -36,7 +36,10 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     """Unload a config entry."""
-    unload_ok = await hass.config_entries.async_unload_platforms(entry, PLATFORMS)
+    unload_ok = await hass.config_entries.async_unload_platforms(
+        entry,
+        PLATFORMS
+    )
     if unload_ok:
         await hass.data[DOMAIN][entry.unique_id].disconnect()
         hass.data[DOMAIN].pop(entry.unique_id)
