cbae27516971fe5edaa94e6b624bcb6bfe5478eb
Cody C <50791984+codyc1515@users.noreply.github.com>
2023-10-25 13:55:38 +1300

    Update config_flow.py

--- custom_components/delonghi_primadonna/config_flow.py
+++ custom_components/delonghi_primadonna/config_flow.py
@@ -17,10 +17,9 @@ _LOGGER = logging.getLogger(__name__)
 STEP_USER_DATA_SCHEMA = voluptuous.Schema(
     {
         voluptuous.Required(
-            CONF_NAME,
-            description={'suggested_value': 'My Precious'}
+            CONF_NAME, description={"suggested_value": "My Precious"}
         ): str,
-        voluptuous.Required(CONF_MAC): str
+        voluptuous.Required(CONF_MAC): str,
     }
 )
 
@@ -38,9 +37,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """Handle the bluetooth discovery step."""
         _LOGGER.info(
-            'Discovered Delonghi device: %s %s',
+            "Discovered Delonghi device: %s %s",
             discovery_info.address,
-            discovery_info.name)
+            discovery_info.name,
+        )
 
         await self.async_set_unique_id(discovery_info.address)
         self._abort_if_unique_id_configured()
@@ -48,13 +48,11 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         self._schema = voluptuous.Schema(
             {
                 voluptuous.Required(
-                    CONF_NAME,
-                    description={'suggested_value': discovery_info.name}
+                    CONF_NAME, description={"suggested_value": discovery_info.name}
                 ): str,
                 voluptuous.Required(
-                    CONF_MAC,
-                    description={'suggested_value': discovery_info.address}
-                ): str
+                    CONF_MAC, description={"suggested_value": discovery_info.address}
+                ): str,
             }
         )
 
@@ -66,13 +64,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
         """Handle the initial step."""
 
         if user_input is None:
-            return self.async_show_form(
-                step_id='user', data_schema=self._schema
-            )
+            return self.async_show_form(step_id="user", data_schema=self._schema)
         else:
             await self.async_set_unique_id(user_input[CONF_MAC])
             self._abort_if_unique_id_configured()
 
-            return self.async_create_entry(
-                title=user_input[CONF_NAME],
-                data=user_input)
+            return self.async_create_entry(title=user_input[CONF_NAME], data=user_input)
