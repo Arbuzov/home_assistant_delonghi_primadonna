a923ab5cac9b5757162ae5221cc58c3ce2edf8a5
Cody C <50791984+codyc1515@users.noreply.github.com>
2023-10-25 14:08:55 +1300

    Update button.py

--- custom_components/delonghi_primadonna/button.py
+++ custom_components/delonghi_primadonna/button.py
@@ -8,7 +8,9 @@ from .device import AvailableBeverage, DelonghiDeviceEntity, DelongiPrimadonna
 
 
 async def async_setup_entry(
-    hass: HomeAssistant, entry: ConfigEntry, async_add_entities: AddEntitiesCallback
+    hass: HomeAssistant,
+    entry: ConfigEntry,
+    async_add_entities: AddEntitiesCallback
 ):
     delongh_device: DelongiPrimadonna = hass.data[DOMAIN][entry.unique_id]
     async_add_entities([DelongiPrimadonnaPowerButton(delongh_device, hass)])
