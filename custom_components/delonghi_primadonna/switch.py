3c50ee1b4193add9ec8fa58cedae78dc8ea6a023
Cody C <50791984+codyc1515@users.noreply.github.com>
2023-10-27 08:42:01 +1300

    Fix isort in switch.py

--- custom_components/delonghi_primadonna/switch.py
+++ custom_components/delonghi_primadonna/switch.py
@@ -1,9 +1,9 @@
 from typing import Any
 
 from homeassistant.config_entries import ConfigEntry
+from homeassistant.const import EntityCategory
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers.entity import ToggleEntity
-from homeassistant.const import EntityCategory
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 
 from .const import DOMAIN
