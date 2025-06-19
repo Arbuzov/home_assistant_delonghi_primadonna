55eaef16f1fb8a4befe5f11e6e1254179455f25e
Arbuzov Sergey <info@whitediver.com>
2025-06-20 02:09:04 +0300

    Fix CONF_MODEL import

--- custom_components/delonghi_primadonna/device.py
+++ custom_components/delonghi_primadonna/device.py
@@ -10,7 +10,7 @@ from enum import IntFlag
 from bleak import BleakClient
 from bleak.exc import BleakDBusError, BleakError
 from homeassistant.components import bluetooth
-from homeassistant.const import CONF_MAC, CONF_NAME
+from homeassistant.const import CONF_MAC, CONF_MODEL, CONF_NAME
 from homeassistant.core import HomeAssistant
 from homeassistant.helpers import device_registry as dr
 
@@ -20,11 +20,10 @@ from .const import (AMERICANO_OFF, AMERICANO_ON, AVAILABLE_PROFILES,
                     BYTES_WATER_HARDNESS_COMMAND,
                     BYTES_WATER_TEMPERATURE_COMMAND, COFFE_OFF, COFFE_ON,
                     COFFEE_GROUNDS_CONTAINER_DETACHED,
-                    COFFEE_GROUNDS_CONTAINER_FULL, CONF_MODEL,
-                    CONTROLL_CHARACTERISTIC, DEBUG, DEVICE_READY,
-                    DEVICE_TURNOFF, DOMAIN, DOPPIO_OFF, DOPPIO_ON,
-                    ESPRESSO2_OFF, ESPRESSO2_ON, ESPRESSO_OFF, ESPRESSO_ON,
-                    HOTWATER_OFF, HOTWATER_ON, LONG_OFF, LONG_ON,
+                    COFFEE_GROUNDS_CONTAINER_FULL, CONTROLL_CHARACTERISTIC,
+                    DEBUG, DEVICE_READY, DEVICE_TURNOFF, DOMAIN, DOPPIO_OFF,
+                    DOPPIO_ON, ESPRESSO2_OFF, ESPRESSO2_ON, ESPRESSO_OFF,
+                    ESPRESSO_ON, HOTWATER_OFF, HOTWATER_ON, LONG_OFF, LONG_ON,
                     NAME_CHARACTERISTIC, START_COFFEE, STEAM_OFF, STEAM_ON,
                     WATER_SHORTAGE, WATER_TANK_DETACHED)
 
